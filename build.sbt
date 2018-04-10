d74ab67237e1dda273f9994511d4df63ee24d286
Emily Pillmore <emilyp@cakesolutions.net>
2018-04-10 16:14:41 -0400

    minimal build

--- build.sbt
+++ build.sbt
@@ -13,17 +13,27 @@ lazy val commonSettings = List(
   "-Xexperimental" // SAM types in 2.11
 )
 
-lazy val ioeffect = (project in file("ioeffect"))
-  .settings(name := "scalaz-ioeffect")
+lazy val root = (project in file ("."))
   .settings(organization := "org.scalaz.ioeffect")
   .settings(description := "The Scalaz IO Monad")
   .settings(scalaVersion := "2.12.5")
+  .aggregate(ioeffect, test)
+
+
+lazy val ioeffect = (project in file("ioeffect"))
+  .settings(name := "scalaz-ioeffect")
   .settings(scalacOptions := commonSettings)
   .settings(libraryDependencies += "org.scalaz" %% "scalaz-core" % "7.2.21")
 
-lazy val tests = (project in file("test"))
+lazy val test = (project in file("test"))
   .settings(name := "scalaz-ioeffect-test")
+  .settings(scalacOptions := commonSettings)
   .settings(noPublish:_*)
+  .settings(libraryDependencies ++= List(
+      "org.specs2" %% "specs2-core" % "4.0.0" % "test",
+      "org.specs2" %% "specs2-matcher-extra" % "4.0.0" % "test"
+    )
+  )
   .dependsOn(ioeffect)
 
 lazy val noPublish = Seq(
---
+++ test/src/test/scala/scalaz/ioeffect/MVarTests.scala
@@ -0,0 +1,121 @@
+package scalaz.ioeffect
+
+import org.specs2.Specification
+import org.specs2.concurrent.ExecutionEnv
+import org.specs2.specification.AroundTimeout
+import org.specs2.specification.core.SpecStructure
+
+import scala.concurrent.ExecutionContext
+import scalaz._
+
+class MVarTests(implicit ee: ExecutionEnv)
+    extends Specification
+    with AroundTimeout {
+  def getEc = ExecutionContext.Implicits.global
+
+  def is: SpecStructure =
+    s2"""
+         MVar Specification
+           have deterministic single take/put behavior            $singleMVarEmptyTest
+           have deterministic single take/put behavior on empty   $singleMVarTest
+           succeed on peek non empty mvar                         $peekOnNonEmpty
+           fail on peek on empty mvar                             $peekOnEmpty
+           succeed on tryput on an empty mvar                     $tryPutOnEmpty
+           fail on tryput on a nonempty mvar                      $tryPutOnEmpty
+           succeed on tryTake on a non empty mvar                 $tryTakeOnNonEmpty
+           fail on tryTake on empty mvar                          $tryTakeOnEmpty
+           have deterministic sequential take/put behaviour       $sequentialMVarTest
+      """
+
+  def singleMVarEmptyTest = {
+    val ioAction = for {
+      mvar <- MVar.newEmptyMVar[Int](getEc)
+      _ <- mvar.put(1)
+      a <- mvar.read
+      b <- mvar.take
+    } yield a -> b
+
+    ioAction.unsafePerformIO() must_== (1 -> 1)
+  }
+
+  def singleMVarTest = {
+    val ioAction = for {
+      mvar <- MVar.newMVar(1)(getEc)
+      a <- mvar.read
+      b <- mvar.take
+    } yield a -> b
+
+    ioAction.unsafePerformIO() must_== (1 -> 1)
+  }
+
+  def tryPutOnEmpty = {
+    val ioAction = for {
+      mvar <- MVar.newEmptyMVar[Int](getEc)
+      put1 <- mvar.tryPut(1)
+      put2 <- mvar.tryPut(2)
+    } yield put1 && !put2
+
+    ioAction.unsafePerformIO() must_== true
+  }
+
+  def tryPutOnNonEmpty = {
+    val ioAction = for {
+      mvar <- MVar.newMVar[Int](1)(getEc)
+      res <- mvar.tryPut(2)
+    } yield res
+
+    ioAction.unsafePerformIO() must_== false
+  }
+
+  def tryTakeOnEmpty = {
+    val ioAction = for {
+      mvar <- MVar.newEmptyMVar[Int](getEc)
+      get1 <- mvar.tryTake
+    } yield get1
+
+    ioAction.unsafePerformIO() must_== Maybe.empty
+  }
+
+  def tryTakeOnNonEmpty = {
+    val ioAction = for {
+      mvar <- MVar.newMVar[Int](1)(getEc)
+      get1 <- mvar.tryTake
+      get2 <- mvar.tryTake
+    } yield get1 -> get2
+
+    ioAction.unsafePerformIO() must_== (Maybe.just(1) -> Maybe.empty)
+  }
+
+
+  def peekOnEmpty = {
+    val ioAction = for {
+      mvar <- MVar.newEmptyMVar[Int](getEc)
+      get2 <- mvar.peek
+    } yield get2
+
+    ioAction.unsafePerformIO() must_== Maybe.empty
+  }
+
+  def peekOnNonEmpty = {
+    val ioAction = for {
+      mvar <- MVar.newMVar[Int](1)(getEc)
+      get2 <- mvar.peek
+    } yield get2
+
+    ioAction.unsafePerformIO() must_== Maybe.just(1)
+  }
+
+  def sequentialMVarTest = {
+
+    val ioAction = for {
+      mvar <- MVar.newEmptyMVar[Int](getEc)
+      _ <- mvar.put(1)
+      _ <- mvar.put(2).fork
+      a <- mvar.take
+      b <- mvar.take
+    } yield (a -> b)
+
+    ioAction.unsafePerformIO() must_== (1 -> 2)
+  }
+
+}
