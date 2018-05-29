426e507429f319506b92cb385b71b6d9059406d2
Filippo Mariotti <filippo.mariotti@gmail.com>
2018-05-29 02:40:46 +0100

    Minor version update

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -9,7 +9,7 @@ import scalafix.sbt.ScalafixPlugin, ScalafixPlugin.autoImport._
 
 object ProjectKeys {
   def KindProjector =
-    addCompilerPlugin("org.spire-math" %% "kind-projector" % "0.9.6")
+    addCompilerPlugin("org.spire-math" %% "kind-projector" % "0.9.7")
 
   def MonadicFor =
     addCompilerPlugin("com.olegpy" %% "better-monadic-for" % "0.2.4")
@@ -83,7 +83,7 @@ object ProjectPlugin extends AutoPlugin {
       // WORKAROUND https://github.com/ghik/silencer/issues/7
       scalacOptions -= "-Ywarn-dead-code",
       libraryDependencies ++= Seq(
-        "org.scalaz" %% "scalaz-core"          % "7.2.22",
+        "org.scalaz" %% "scalaz-core"          % "7.2.23",
         "org.specs2" %% "specs2-core"          % specs2Version % "test",
         "org.specs2" %% "specs2-matcher-extra" % specs2Version % "test"
       )
