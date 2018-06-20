b301684a1b2c422034eed58db17110cd3cf1c656
Sam Halliday <sam.halliday@gmail.com>
2018-06-20 11:47:29 +0100

    Plus[IO] and MonadPlus[IO]

--- ioeffect/src/main/scala/scalaz/ioeffect/IOInstances.scala
+++ ioeffect/src/main/scala/scalaz/ioeffect/IOInstances.scala
@@ -2,14 +2,19 @@
 package scalaz
 package ioeffect
 
-trait IOInstances extends IOInstances1 {
+abstract class IOInstances extends IOInstances1 {
   // cached for efficiency
-  implicit val taskInstances: MonadError[Task, Throwable] with BindRec[Task] = new IOMonadError[Throwable]
+  implicit val taskInstances: MonadError[Task, Throwable] with BindRec[Task] with Plus[Task] =
+    new IOMonadError[Throwable] with IOPlus[Throwable]
 }
 
-sealed trait IOInstances1 {
-  implicit def ioInstances[E]: MonadError[IO[E, ?], E] with BindRec[IO[E, ?]] with Bifunctor[IO] =
-    new IOMonadError[E] with IOBifunctor
+sealed abstract class IOInstances1 extends IOInstance2 {
+  implicit def ioInstances[E]: MonadError[IO[E, ?], E] with BindRec[IO[E, ?]] with Bifunctor[IO] with Plus[IO[E, ?]] =
+    new IOMonadError[E] with IOPlus[E] with IOBifunctor
+}
+
+sealed abstract class IOInstance2 {
+  implicit def ioMonadPlus[E: Monoid]: MonadPlus[IO[E, ?]] with BindRec[IO[E, ?]] = new IOMonadPlus[E]
 }
 
 private class IOMonad[E] extends Monad[IO[E, ?]] with BindRec[IO[E, ?]] {
@@ -23,6 +28,16 @@ private class IOMonad[E] extends Monad[IO[E, ?]] with BindRec[IO[E, ?]] {
 private class IOMonadError[E] extends IOMonad[E] with MonadError[IO[E, ?], E] {
   override def handleError[A](fa: IO[E, A])(f: E => IO[E, A]): IO[E, A] = fa.catchAll(f)
   override def raiseError[A](e: E): IO[E, A]                            = IO.fail(e)
+}
+
+// lossy, throws away errors using the "first success" interpretation of Plus
+private trait IOPlus[E] extends Plus[IO[E, ?]] {
+  override def plus[A](a: IO[E, A], b: => IO[E, A]): IO[E, A] = a.catchAll(_ => b)
+}
+
+// annoyingly, we just need M.zero
+private class IOMonadPlus[E](implicit M: Monoid[E]) extends IOMonadError[E] with IOPlus[E] with MonadPlus[IO[E, ?]] {
+  override def empty[A]: IO[E, A] = raiseError(M.zero)
 }
 
 private trait IOBifunctor extends Bifunctor[IO] {
